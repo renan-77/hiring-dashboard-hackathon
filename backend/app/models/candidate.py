c2df9473e82d5cee1be332fbbaecd4d6cf1a0ed8
renan-77 <renanmonteiroft@gmail.com>
2021-04-29 12:18:37 +0100

    Added candidate model

---
+++ backend/app/models/candidate.py
@@ -0,0 +1,13 @@
+from mongoengine import ObjectIdField, Document
+from app import db
+
+
+class Candidate(Document):
+    """ Candidate class for declaring a model for Candidate collection """
+    _id = ObjectIdField()
+    name = db.StringField()
+    email = db.StringField(unique=True)
+    personal_token = db.StringField()
+    video_interview_path = db.StringField()
+    cv_path = db.StringField()
+    password = db.StringField()
