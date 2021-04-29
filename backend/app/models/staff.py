e1e23d94a6db7870574e8da644f77f389f96dedb
renan-77 <renanmonteiroft@gmail.com>
2021-04-29 12:18:09 +0100

    Added staff model

--- backend/app/models/staff.py
+++ backend/app/models/staff.py
@@ -2,9 +2,10 @@ from mongoengine import ObjectIdField, Document
 from app import db
 
 
-class User(Document):
+class Staff(Document):
     """ User class for declaring a model for User collection """
     _id = ObjectIdField()
     name = db.StringField()
     email = db.StringField(unique=True)
     password = db.StringField()
+    isStaff = db.BooleanField()
