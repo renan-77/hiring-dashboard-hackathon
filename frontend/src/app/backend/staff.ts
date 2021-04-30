9f3483ab0299e41b3f36dbee8fe0d0ac27fb9ad6
renan-77 <renanmonteiroft@gmail.com>
2021-04-30 15:42:40 +0100

    Added mock backend and created mock data and script to create mock data

---
+++ frontend/src/app/backend/staff.ts
@@ -0,0 +1,23 @@
+export let staff = [
+    {
+        '_id': '608bdd61129b9297d21882ed',
+        'name': 'Renan',
+        'email': 'renan@dell.com',
+        'password': 'renan123',
+        'isStaff': true
+    },
+    {
+        '_id': '608bdd61129b9297d21882ed',
+        'name': 'Dovydas',
+        'email': 'dovydas@dell.com',
+        'password': 'dovydas123',
+        'isStaff': true
+    },
+    {
+        '_id': '608bdd61129b9297d21882ed',
+        'name': 'Michael',
+        'email': 'michael@dell.com',
+        'password': 'michael123',
+        'isStaff': true
+    }
+]
